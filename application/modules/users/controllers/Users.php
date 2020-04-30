cd520531ad78a468691d13c49e6989026a3667dc
Shipan Mazumder <shipanmazumder@gmail.com>
2020-05-01 02:27:03 +0600

    some changes

--- application/modules/users/controllers/Users.php
+++ application/modules/users/controllers/Users.php
@@ -1,40 +1,40 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * Class Users
  * @property Users_Model $users
  */
-class Users extends MY_Controller {
-    public $data=array();
+class Users extends MY_Controller
+{
+	public $data = array();
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('Users_Model','users',true);
-        $this->session->set_userdata('top_menu', 'users');
-        $this->session->set_userdata('sub_menu', 'users');
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Users_Model', 'users', true);
+		$this->session->set_userdata('top_menu', 'users');
+		$this->session->set_userdata('sub_menu', 'users');
+	}
 
 	public function index()
-    {
-        checkPermission("users",VIEW);
+	{
+		checkPermission("users", VIEW);
 
-        $this->layout->title("Users");
-        $this->layout->view('index',$this->data);
-    }
-    //library ajax view
-    public function view()
-    {
-        if($_GET)
-        {
-        	$search_key=trim($this->input->get("search_key"));
-        	$filter_by=trim($this->input->get("filter_by"));
-        	$offset=($this->uri->segment(3))? $this->uri->segment(3):0;
-        	$this->load->library('pagination');
+		$this->layout->title("Users");
+		$this->layout->view('index', $this->data);
+	}
+	//library ajax view
+	public function view()
+	{
+		if ($_GET) {
+			$search_key = trim($this->input->get("search_key"));
+			$filter_by = trim($this->input->get("filter_by"));
+			$offset = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+			$this->load->library('pagination');
 			$config['base_url']    = base_url('users/view');
-			$config['total_rows']  = $this->users->get_all_users("","",$search_key,$filter_by,true);
+			$config['total_rows']  = $this->users->get_all_users("", "", $search_key, $filter_by, true);
 			$config['per_page']    = QUESTION_PER_PAGE;
 			$config['full_tag_open'] = '<ul class="pagination">';
 			$config['full_tag_close'] = '</ul>';
@@ -52,61 +52,55 @@ class Users extends MY_Controller {
 			$config['last_tagl_close'] = '</a></li>';
 			$config['attributes'] = array('class' => 'page-link');
 
-			 $this->pagination->initialize($config);
-			$this->data['all_users']=$this->users->get_all_users(QUESTION_PER_PAGE,$offset,$search_key,$filter_by);
-			$this->data['serial']=$offset;
-			$this->data['total_rows']=$config['total_rows'];
-			$result=$this->load->view("users-view",$this->data,true);
+			$this->pagination->initialize($config);
+			$this->data['all_users'] = $this->users->get_all_users(QUESTION_PER_PAGE, $offset, $search_key, $filter_by);
+			$this->data['serial'] = $offset;
+			$this->data['total_rows'] = $config['total_rows'];
+			$result = $this->load->view("users-view", $this->data, true);
 			echo json_encode($result);
 			exit;
-        }else
-		{
+		} else {
 			show_404();
 		}
-    }
+	}
 
 
 	/**
 	 * @param null $id
 	 */
 	public function delete($id = null)
-    {
-        $result = $this->users->get_single("users", array("id" => $id));
-        if (isset($result)) {
-            $this->users->trans_start();
-            $this->users->delete("users",array("id" => $id));
+	{
+		$result = $this->users->get_single("users", array("id" => $id));
+		if (isset($result)) {
+			$this->users->trans_start();
+			$this->users->delete("users", array("id" => $id));
 			@unlink($result->picture);
 			@unlink($result->cover_picture);
-            $this->users->trans_complete();
-            if($this->users->trans_status()){
-            	setMessage("msg", "success", " Deleted Successfuly");
-			}else
-			{
-            	setMessage("msg", "danger", " Not Deleted");
-
+			$this->users->trans_complete();
+			if ($this->users->trans_status()) {
+				setMessage("msg", "success", " Deleted Successfuly");
+			} else {
+				setMessage("msg", "danger", " Not Deleted");
 			}
-        }
-        redirect('users');
-    }
-    public function control($id = null)
-    {
-        $result = $this->users->get_single("users", array("id" => $id));
-        $status = 0;
-        if (isset($result)) {
-        	if($result->status==0)
-			{
-				$status=1;
-            	setMessage("msg", "success", "Approved Successfuly");
+		}
+		redirect('users');
+	}
+	public function control($id = null)
+	{
+		$result = $this->users->get_single("users", array("id" => $id));
+		$status = 0;
+		if (isset($result)) {
+			if ($result->status == 0) {
+				$status = 1;
+				setMessage("msg", "success", "Approved Successfuly");
+			} else if ($result->status == 1) {
+				$status = 2;
+				setMessage("msg", "success", "Disable Successfuly");
 			}
-        	else if($result->status==1)
-			{
-				$status=2;
-            	setMessage("msg", "success", "Disable Successfuly");
-			}
-            $this->users->update("users",array("status"=>$status),array("id" => $id));
-        }
-        redirect('users');
-    }
+			$this->users->update("users", array("status" => $status), array("id" => $id));
+		}
+		redirect('users');
+	}
 }
 
 /* End of file Users.php */
